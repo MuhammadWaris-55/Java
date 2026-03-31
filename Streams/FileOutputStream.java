24e59e27b236a8ae401b16b13badb44c304f9931
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-03-31 10:49:19 +0500

    Make new file and add Data using FileOutputStream

---
+++ Streams/FileOutputStream.java
@@ -0,0 +1,21 @@
+//This code is used to create a file and write characters into it using FileOutputStream.
+//BufferedOutputStream stores data temporarily and writes it more efficiently to the file when closed.
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class OutputStream {
+    public static void main(String[] args) {
+        FileOutputStream os=null;
+        try{
+            os=new FileOutputStream("my Data.txt");
+            BufferedOutputStream bs=new BufferedOutputStream(os);
+            for (int i = 65; i < 75; i++) {
+                os.write(i);
+            }
+        } catch (IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
