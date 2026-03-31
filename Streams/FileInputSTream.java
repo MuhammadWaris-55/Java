88d2a8162c390cbe514df566ccd1295af561076e
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-03-31 10:35:43 +0500

    Read the Data from existing file using FileInputStream

---
+++ Streams/FileInputSTream.java
@@ -0,0 +1,18 @@
+//This code is reading a text file character by character using FileInputStream.
+//this code only reads data from an already existing file.
+import java.io.FileInputStream;
+public class Main {
+    public static void main(String[] args) {
+        FileInputStream is=null;
+        try{
+            is= new FileInputStream("my data.txt");
+            int next;
+            while((next=is.read())!=-1){
+                System.out.println("next = "+ (char)next);
+            }
+            is.close();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
