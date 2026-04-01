7356ffea7b44c83c1c49ec7e4dcec5d876484385
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-04-01 11:08:15 +0500

    Make one button in gui

---
+++ GUI/button.java
@@ -0,0 +1,23 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+
+public class Main {
+    JFrame jf;
+
+    public Main(){
+        jf = new JFrame("My Window");
+        JButton btn = new JButton("Say Hello");
+
+        jf.setLayout(new FlowLayout());
+        jf.add(btn);
+
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setSize(400, 400);
+        jf.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
