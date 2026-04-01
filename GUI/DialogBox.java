f6d55e51f745d4605e7c17c54ad0b5bd9391ae97
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-04-01 15:55:48 +0500

    Make DialogBox Some text written

---
+++ GUI/DialogBox.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+    JFrame f;
+
+    Main(){
+        f = new JFrame();
+        JOptionPane.showMessageDialog(f,"Hello World!" , "Info", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
