019440ac46780d7fa810a4dbae77f18589475019
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-04-16 00:05:26 +0500

    Add Comments

--- Park Association with composition/Main.java
+++ Park Association with composition/Main.java
@@ -1,16 +1,21 @@
 import java.util.ArrayList;
 public class Main{
     public static void main(String[] args){
+
+        // Setup rides and parks
         ArrayList<String> Rides = new ArrayList<>();
         Rides.add("ride-1");
         Rides.add("ride-2");
         Park p1 = new Park("Adventure Land");
         Park p2 = new Park("My Park");
         p1.set_rides(Rides);
+
+        // Setup visitors and assign to parks
         Visitors v1 = new Visitors("Waris");
-        Visitors v2 = new Visitors("Ali" );
+        Visitors v2 = new Visitors("Ali");
         p1.set_visitors(v1);
         p2.set_visitors(v2);
+
         System.out.println(p1.get_name());
         System.out.println(p1.get_rides());
     }
