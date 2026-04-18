ae68d8efdf5ce1b0df4a786cad4671c3d4646009
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-04-18 16:07:03 +0500

    Add Comments

--- GUI/button.java
+++ GUI/button.java
@@ -11,10 +11,10 @@ public class Main {
         jf = new JFrame("My Window");
         JButton btn = new JButton("Say Hello");
 
-        jf.setLayout(new FlowLayout());
+        jf.setLayout(new FlowLayout()); // Arrange components left to right
         jf.add(btn);
 
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit app on window close
         jf.setSize(400, 400);
         jf.setVisible(true);
     }
