0e89d2218799f834eeccb7171847edc83d947f1e
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-04-18 16:07:56 +0500

    Add Comments

--- GUI/DialogBox.java
+++ GUI/DialogBox.java
@@ -5,10 +5,11 @@ public class Main {
 
     Main(){
         f = new JFrame();
+        // Show a dialog box with an info icon
         JOptionPane.showMessageDialog(f,"Hello World!" , "Info", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static void main(String[] args) {
         new Main();
     }
-}  
+}
