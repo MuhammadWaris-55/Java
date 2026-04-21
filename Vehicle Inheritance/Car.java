f5fe6e81232fb8f715c224b358348d51d30fadaa
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-04-21 16:39:37 +0500

    Make Car class

---
+++ Vehicle Inheritance/Car.java
@@ -0,0 +1,23 @@
+public class Car extends Vehicle{
+    private int NoofDoors;
+    public Car(String brand , String model , float fueltank , int NoofDoors){
+        super(brand, model, fueltank);
+        this.NoofDoors = NoofDoors;
+    }
+    public Car(Car x){
+        super(x.brand , x.model , x.fueltank);
+        this.NoofDoors = x.NoofDoors;
+    }
+    public void drive(){
+        System.out.println("The Car is Been Driven");
+    }
+    public void opentrunk(){
+        System.out.println("The trunk of car is open");
+    }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("The No of Doors are: " + NoofDoors);
+    }
+}
