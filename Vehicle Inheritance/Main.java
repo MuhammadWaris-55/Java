226da37ccb5ed4066ab09242da0c5bd6a9afdd9f
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-04-21 16:41:11 +0500

    Add Main class

---
+++ Vehicle Inheritance/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle("BMW", "m5 cs" , 60);
+        v1.display();
+        Car c1 = new Car("Mercedes" , "c63" , 66 , 4);
+        c1.display();
+        MotorBike m1 = new MotorBike("Kawasaki" , "H2r" , 20 , false);
+        m1.display();
+        Car c2 = new Car(c1);
+        c2.display();
+    }
+}
