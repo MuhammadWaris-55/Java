65e2c55de16e19e35b4cb77bcc56cb6d5e0c2e51
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-04-27 21:49:13 +0500

    Make Abstract Class

---
+++ Interface/Customer Order/CustomerOrder.java
@@ -0,0 +1,48 @@
+public abstract class CustomerOrder {
+
+    private String orderID;
+    private double subtotalAmount;
+    private PaymentProcessor currentProcessor;
+
+    public CustomerOrder(String orderID, double subtotalAmount) {
+        this.orderID = orderID;
+        this.subtotalAmount = subtotalAmount;
+    }
+
+    public void setPaymentProcessor(PaymentProcessor method) {
+        this.currentProcessor = method;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public abstract void calculateTotalWithFees();
+
+    public final void processCheckout() {
+        System.out.println("=== Starting Checkout for Order: " + orderID + " ===");
+        validateOrder();
+        calculateTotalWithFees();
+        if (currentProcessor != null) {
+            currentProcessor.processPayment(subtotalAmount);
+        } else {
+            System.out.println("WARNING: No payment processor set!");
+        }
+        System.out.println("=== Checkout Complete ===\n");
+    }
+
+    protected void validateOrder() {
+        if (subtotalAmount <= 0) {
+            throw new IllegalStateException("Subtotal must be greater than zero.");
+        }
+        System.out.println("Order valid. Subtotal: $" + subtotalAmount);
+    }
+
+    public double getSubtotalAmount() {
+        return subtotalAmount;
+    }
+
+    protected void setSubtotalAmount(double subtotalAmount) {
+        this.subtotalAmount = subtotalAmount;
+    }
+}
