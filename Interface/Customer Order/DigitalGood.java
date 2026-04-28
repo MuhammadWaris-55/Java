70a46b7c971db19fd7fbdf81446b4c131d389111
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-04-28 18:18:25 +0500

    Fix Bugs

--- Interface/Customer Order/DigitalGood.java
+++ Interface/Customer Order/DigitalGood.java
@@ -9,7 +9,7 @@ public class DigitalGood extends CustomerOrder {
 
     @Override
     public void calculateTotalWithFees() {
-        double digitalTax = getSubtotalAmount() * 0.05;
+        double digitalTax = getSubtotalAmount() * 0.05; // 5% digital tax
         double total = getSubtotalAmount() + digitalTax;
         System.out.println("Digital Good - Download Link: " + downloadLink);
         System.out.println("Subtotal: $" + getSubtotalAmount() + " + Digital Tax (5%): $" + digitalTax + " = Total: $" + total);
