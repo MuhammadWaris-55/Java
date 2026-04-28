8decaadffcc409e45827138b46349ca506e9f02f
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-04-28 18:19:17 +0500

    Fix Bugs

--- Interface/Customer Order/CreditCardProcessor.java
+++ Interface/Customer Order/CreditCardProcessor.java
@@ -9,6 +9,7 @@ public class CreditCardProcessor implements PaymentProcessor {
     @Override
     public void processPayment(double amount) {
         System.out.println("CreditCardProcessor: Authorizing via bank check...");
+        // Show only last 4 digits for security
         System.out.println("Card ending in " + cardNumber.substring(cardNumber.length() - 4) + " charged $" + amount + " [APPROVED]");
     }
 }
