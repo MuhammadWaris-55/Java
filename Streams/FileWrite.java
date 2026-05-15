fb75794365b73636d8c084fc577f416f9d382066
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-16 02:03:11 +0500

    Learn TextField

---
+++ Streams/FileWrite.java
@@ -0,0 +1,21 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+public class FileWrite {
+    public static void main(String[] args) {
+
+
+        try(FileWriter write = new FileWriter("testing.txt")){
+            write.write("I Like BMW");
+            System.out.println("File Has been Written");
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File Ji Location Nathi Mile");
+        }
+        catch (IOException e){
+            System.out.println("Kon Likhandam Cha Kande");
+        }
+    }
+}
