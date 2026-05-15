9a827d5503b3c4d719c9cfa683d85d15b32068e9
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-05-16 01:32:17 +0500

    Learn TextField

---
+++ GUI/TextField.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TextField {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        JTextField textfield = new JTextField();
+        textfield.setPreferredSize(new Dimension(300, 100));
+        frame.add(textfield);
+
+        JButton btn = new JButton("SUBMIT");
+        // Lambda expression - no extra class needed
+        btn.addActionListener(e -> System.out.println("Waris Codes"));
+        frame.add(btn);
+
+        frame.setVisible(true);
+    }
+}
